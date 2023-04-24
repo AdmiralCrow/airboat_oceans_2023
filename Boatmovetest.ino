4e2ce290fd5a91effe2656eadc99b0caef24797f
Anthony Devesa <67215398+AdmiralCrow@users.noreply.github.com>
2023-04-24 02:59:11 -0400

    Update Boatmovetest.ino
    Old code was using a 4 pin system to control the boat updated to a 2 pin system code and works perfectly

--- Boatmovetest.ino
+++ Boatmovetest.ino
@@ -1,52 +1,122 @@
-// C++ code
-//
-
-const int left_motor_a = 4;
-const int left_motor_b = 5;
-const int right_motor_a = 6;
-const int right_motor_b = 7;
-const int enable_a = 10;
-const int enable_b = 11;
+// Define the pins for controlling the DC motors
+const int motor_a = 9;// output2
+const int motor_b = 11;//output1
+const int enable = 6;
 
 void setup()
 {
-  pinMode(left_motor_a, OUTPUT); // left motor: CCW
-  pinMode(left_motor_b, OUTPUT); // left motor: CW
-  pinMode(right_motor_a, OUTPUT); // right motor: CCW
-  pinMode(right_motor_b, OUTPUT); // right motor: CW
-  pinMode(enable_a, OUTPUT);
-  pinMode(enable_b, OUTPUT);
-  Serial.begin(9600);
- 
-}
-
-void forward(int second) {
-  Serial.println("Forward Moving");
-  digitalWrite(right_motor_a, LOW); // This was changed from HIGH to LOW do boat moving backwards. 
-  digitalWrite(right_motor_b, HIGH);
-  digitalWrite(left_motor_a, LOW); // same change as above
-  digitalWrite(left_motor_b, HIGH);
-  analogWrite(enable_a, 200);
-  analogWrite(enable_b, 200);
-  delay(second * 1000);
+  // Set the motor control pins to output mode
+  pinMode(motor_a, OUTPUT);
+  pinMode(motor_b, OUTPUT);
+  pinMode(enable, OUTPUT);
   
+  // Initialize Serial communication at a baud rate of 9600
+  Serial.begin(9600);
 }
 
+// Function for moving the robot forward for a specified duration
+void forward(int seconds) {
+  // Print a message to Serial to indicate the robot is moving forward
+  Serial.println("Moving forward");
+
+  // Set the direction signals for the motors to rotate them in the forward direction
+  digitalWrite(motor_a, HIGH);
+  digitalWrite(motor_b, LOW);
+
+  // Set the speed control signals for the motors using analogWrite function
+  analogWrite(enable, 200);
+
+  // Wait for the specified duration of time using the delay function
+  delay(seconds * 1000);
+}
+
+// Function for stopping the robot
 void stop() {
-  Serial.println("Stopping Motors");
-  digitalWrite(right_motor_a, LOW);
-  digitalWrite(right_motor_b, LOW);
-  digitalWrite(left_motor_a, LOW);
-  digitalWrite(left_motor_b, LOW);
-  analogWrite(enable_a, 0);
-  analogWrite(enable_b, 0);
+  // Print a message to Serial to indicate the robot is stopping
+  Serial.println("Stopping");
+
+  // Set the direction signals for the motors to stop
+  digitalWrite(motor_a, LOW);
+  digitalWrite(motor_b, LOW);
+
+  // Set the speed control signals for the motors to 0
+  analogWrite(enable, 0);
 }
 
-const int seconds = 2;
+// Function for moving the robot backward for a specified duration
+void backward(int seconds) {
+  // Print a message to Serial to indicate the robot is moving backward
+  Serial.println("Moving backward");
+
+  // Set the direction signals for the motors to rotate them in the backward direction
+  digitalWrite(motor_a, LOW);
+  digitalWrite(motor_b, HIGH);
+
+  // Set the speed control signals for the motors using analogWrite function
+  analogWrite(enable, 200);
+
+  // Wait for the specified duration of time using the delay function
+  delay(seconds * 1000);
+}
+
+// Function for turning the robot left for a specified duration
+void turn_left(int seconds) {
+  // Print a message to Serial to indicate the robot is turning left
+  Serial.println("Turning left");
+
+  // Set the direction signals for the motors to turn left
+  digitalWrite(motor_a, LOW);
+  digitalWrite(motor_b, HIGH);
+
+  // Set the speed control signals for the motors using analogWrite function
+  analogWrite(enable, 200);
+
+  // Wait for the specified duration of time using the delay function
+  delay(seconds * 1000);
+}
+
+// Function for turning the robot right for a specified duration
+void turn_right(int seconds) {
+  // Print a message to Serial to indicate the robot is turning right
+  Serial.println("Turning right");
+
+  // Set the direction signals for the motors to turn right
+  digitalWrite(motor_a, HIGH);
+  digitalWrite(motor_b, LOW);
+
+  // Set the speed control signals for the motors using analogWrite function
+  analogWrite(enable, 200);
+
+  // Wait for the specified duration of time using the delay function
+  delay(seconds * 1000);
+}
 
 void loop()
 {
+  // Move forward for 6 seconds
   forward(6);
+
+  // Stop the robot
   stop();
-  delay(seconds * 1000);
+
+  // Wait for 2 seconds before starting the next movement
+  delay(2000);
+
+  // Turn left for 2 seconds
+  turn_left(2);
+
+  // Stop the robot
+  stop();
+
+  // Wait for 2 seconds before starting the next movement
+  delay(2000);
+
+  // Turn right for 2 seconds
+  turn_right(2);
+
+ // Stop the robot
+  stop();
+
+  // Wait for 2 seconds before starting the next movement
+  delay(2000);
 }
