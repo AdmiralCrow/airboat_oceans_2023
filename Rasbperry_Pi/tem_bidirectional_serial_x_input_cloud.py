5a4cda336202b7e3074c880cb2a3adf9e46d0617
Cesar Rojas <croja022@fiu.edu>
2023-07-03 18:10:37 -0400

    Added Trajectory ID

--- Rasbperry_Pi/tem_bidirectional_serial_x_input_cloud.py
+++ Rasbperry_Pi/tem_bidirectional_serial_x_input_cloud.py
@@ -9,7 +9,7 @@ import serial
 from picamera2 import Picamera2
 
 if __name__ == '__main__':
-    uri = "mongodb+srv://marinerobotics:17B9EoUMJnCZNDvz@cluster0.7tv1lgs.mongodb.net/?retryWrites=true&w=majority"
+    uri = "mongodb+srv://db-user:db-password@cluster0.7tv1lgs.mongodb.net/?retryWrites=true&w=majority"
 
     # Create a new client and connect to the server
     client = MongoClient(uri)
@@ -24,6 +24,13 @@ if __name__ == '__main__':
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
         print(e)
+
+    # Initialize trajectory_id from the max value in the collection, or set to 0 if not found
+    max_doc = collection.find_one(sort=[("trajectory_id", -1)])
+    if max_doc and "trajectory_id" in max_doc:
+        trajectory_id = max_doc["trajectory_id"] + 1
+    else:
+        trajectory_id = 0
 
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # Raspberry Pi
     # ser = serial.Serial('/dev/tty.usbmodem14601', 9600, timeout=1)  # Linux/macOS
@@ -66,9 +73,13 @@ if __name__ == '__main__':
                                 'datetime': datetime.now(),
                                 'temperature': temp,
                                 'latitude': lat,
-                                'longitude': lon
+                                'longitude': lon,
+                                'trajectory_id': trajectory_id
                             }
                             collection.insert_one(data)
                     elif command == 'BTN_START':
                         timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
                         picam2.capture_file(timestamp + '.jpg')
+                    elif command == 'BTN_SELECT':
+                        # Increment the trajectory id by 1
+                        trajectory_id += 1
